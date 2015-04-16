38806843c215763c5606dffc94a4d4e0b5fb4cc3
Phạm Kỳ Khôi <daotaolaptrinhsuntech@gmail.com>
2015-04-16 13:55:45 +0700

    Test.rb

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,54 @@
+class	UsersController < ApplicationController
+	before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy ]
+	def index
+		@users = User.all
+	end
+	
+	def new
+		@users = User.new
+	end
+
+	def edit
+		@users = User.find(params[:id])
+	end
+
+	def show
+		@users = User.find(params[:id])
+	end
+
+	def create
+		@users = User.new user_params
+		if @users.save 
+			flash[:success] = "Insert user complete"
+	    redirect_to users_path
+		else
+			render "new"
+		end
+	end
+
+	def update
+		@users = User.find(params[:id])
+    if @users.update(user_params)
+      redirect_to users_path
+    else
+      render 'edit'
+    end
+	end
+
+	def destroy
+		User.find(params[:id]).destroy
+		redirect_to users_path
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	end
+
+	def logged_in_user
+	  unless logged_in?
+	    flash[:danger] = "Please log in take access system"
+	      redirect_to root_path
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   get    'login'   =>    'sessions#new'
   post   'login'   =>    'sessions#create'
   delete 'logout'  =>    'sessions#destroy'
-
+  resources :users
+  get    'signup'   =>    'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
